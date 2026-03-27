caa1c6823d8a02a3d346d552c5290a8be4fe947e
spietzsch007 <spietzsch@ikp.tu-darmstadt.de>
2026-03-27 12:53:19 +0100

    Delete data directory

--- data/convert_bibtex.py
+++
@@ -1,8 +0,0 @@
-import bibtexparser
-import json
-
-with open("data/publications.bib") as bib_file:
-    bib_database = bibtexparser.load(bib_file)
-
-with open("data/publications.json", "w") as json_file:
-    json.dump(bib_database.entries, json_file, indent=2)
